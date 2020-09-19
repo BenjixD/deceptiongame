4096327be366dbe2d584834b22e89e6f3cc0e8a1
Yilun Zhao <yl4zhao@uwaterloo.ca>
2020-09-18 23:36:47 -0400

    First Commit

---
+++ DeceptionGame/Assets/Scripts/FrameRateLimiter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameRateLimiter : MonoBehaviour
+{
+	public int limit;
+    // Start is called before the first frame update
+    void Awake()
+    {
+#if UNITY_EDITOR
+    QualitySettings.vSyncCount = 0;  // VSync must be disabled
+    Application.targetFrameRate = limit;
+#endif
+    }
+}
