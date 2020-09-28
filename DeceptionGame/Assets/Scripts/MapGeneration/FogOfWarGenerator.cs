31c5d1f7040d2de8a890bf134f13531a4504b541
Anthony Tu <antu3199@gmail.com>
2020-09-28 11:48:38 -0400

    Fixed fog of war rework and destiny card

--- DeceptionGame/Assets/Scripts/MapGeneration/FogOfWarGenerator.cs
+++ DeceptionGame/Assets/Scripts/MapGeneration/FogOfWarGenerator.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 // Simple fog of war implementation from https://www.youtube.com/watch?v=iGAdaZ1ICaI
 
-public enum MapVisibility {
+public enum Visibility {
     INVISIBLE = 0,
-    VISIBLE = 1,
-    INVISIBLE_BUT_SEEN_BEFORE = 2
+    INVISIBLE_BUT_SEEN_BEFORE = 1,
+    VISIBLE = 2,
+
+};
+
+public class MapVisibility {
+    public float alpha = 1.0f;
+    public Visibility visibility;
+
+    public MapVisibility(float alpha, Visibility visibility) {
+        this.alpha = alpha;
+        this.visibility = visibility;
+    }
+
+    public void Reset() {
+        this.alpha = 1.0f;
+        this.visibility = Visibility.INVISIBLE;
+    }
 };
 
 public class FogOfWarGenerator : MonoBehaviour {
@@ -27,13 +43,14 @@ public class FogOfWarGenerator : MonoBehaviour {
 	private Vector3[] m_vertices;
 	private Color[] m_colors;
 	
-
     private HashSet<int> seenIndices = new HashSet<int>();
 
     private MapController mapController;
 
     private MapVisibility[] playerMapVisibility;
     private Vector3[] worldPosVertices;
+
+    private List<Vector3> visionLocations = new List<Vector3>();
 
     public void Initialize(MapController controller) {
         this.mapController = controller;
@@ -49,52 +66,76 @@ public class FogOfWarGenerator : MonoBehaviour {
         }
 
         this.CastVisionOn(player.transform.position);
+
+        this.UpdateMapVisibility();
+
+        this.visionLocations.Clear();
+
 	}
 
+    // Casts vision FOR A SINGLE FRAME
     public void CastVisionOn(Vector3 position) {
+        this.visionLocations.Add(position);
+    }
 
-        // TODO: Fix bug
-        Vector3 rayOrigin = position + Vector3.up * maxDistance;
+    // This is where the magic happens for Fog of war
+    private void UpdateMapVisibility() {
 
-		Ray r = new Ray(rayOrigin, position - rayOrigin);
-		RaycastHit hit;
+        for (int i=0; i< m_vertices.Length; i++) {
+            this.playerMapVisibility[i].Reset();
+        }
 
-        Debug.DrawLine(rayOrigin, position, Color.red);
-		if (Physics.Raycast(r, out hit, 1000, fogLayer, QueryTriggerInteraction.Collide)) {
-			for (int i=0; i< m_vertices.Length; i++) {
-				Vector3 v = worldPosVertices[i];
-                MapGenerator.Coord coord = this.mapController.mapGenerator.NearestWorldPointToCoord(v);
-        
+        foreach (Vector3 position in this.visionLocations) {
+            Vector3 rayOrigin = position + Vector3.up * maxDistance;
 
-				float dist = Vector3.SqrMagnitude(v - hit.point);
-				if (dist < m_radiusSqr) {
-                    float attenuation = dist / m_radiusSqr;
-                    
-					//float alpha = Mathf.Min(m_colors[i].a, dist/m_radiusSqr);
-                    float alpha;
-                    if (dist >= m_radiusSqr / 3) {
-                        alpha = attenuation;
+            Ray r = new Ray(rayOrigin, position - rayOrigin);
+            RaycastHit hit;
+
+            Debug.DrawLine(rayOrigin, position, Color.red);
+            if (Physics.Raycast(r, out hit, 1000, fogLayer, QueryTriggerInteraction.Collide)) {
+                for (int i=0; i< m_vertices.Length; i++) {
+                    Vector3 v = worldPosVertices[i];
+                    MapGenerator.Coord coord = this.mapController.mapGenerator.NearestWorldPointToCoord(v);
+            
+
+                    float dist = Vector3.SqrMagnitude(v - hit.point);
+                    if (dist < m_radiusSqr) {
+                        float attenuation = dist / m_radiusSqr;
+                        
+                        //float alpha = Mathf.Min(m_colors[i].a, dist/m_radiusSqr);
+                        float alpha;
+                        if (dist >= m_radiusSqr / 3) {
+                            alpha = attenuation;
+                        } else {
+                            alpha = attenuation * attenuation;
+                        }
+
+                        alpha = Mathf.Min(unseenAlpha, alpha);
+                        
+                        //alpha = 0;
+                        m_colors[i].a = alpha;
+                        seenIndices.Add(i);
+                        this.UpdateVisibilityIfBetter(this.playerMapVisibility, i, Visibility.VISIBLE, alpha);
                     } else {
-                        alpha = attenuation * attenuation;
-                    }
-
-                    alpha = Mathf.Min(unseenAlpha, alpha);
-                     
-                    //alpha = 0;
-					m_colors[i].a = alpha;
-                    seenIndices.Add(i);
-                    this.playerMapVisibility[i] = MapVisibility.VISIBLE;
-				} else {
-                    if (seenIndices.Contains(i)) {
-                        m_colors[i].a = unseenAlpha;
-                        this.playerMapVisibility[i] = MapVisibility.INVISIBLE_BUT_SEEN_BEFORE;
-                    } else {
-                        this.playerMapVisibility[i] = MapVisibility.INVISIBLE;
+                        if (seenIndices.Contains(i)) {
+                            this.UpdateVisibilityIfBetter(this.playerMapVisibility, i,  Visibility.INVISIBLE_BUT_SEEN_BEFORE, unseenAlpha);
+                        } else {
+                            this.UpdateVisibilityIfBetter(this.playerMapVisibility, i,  Visibility.INVISIBLE, 1.0f);
+                        }
                     }
                 }
-			}
-			UpdateColor();
-		}
+            }
+        }
+
+        UpdateColor();
+
+    }
+
+    private void UpdateVisibilityIfBetter(MapVisibility[] map, int index, Visibility visibility, float alpha) {
+        if ( alpha < map[index].alpha ) {
+            map[index].alpha = alpha;
+            map[index].visibility = visibility;
+        }
     }
 
 	
@@ -103,11 +144,14 @@ public class FogOfWarGenerator : MonoBehaviour {
 		m_vertices = m_mesh.vertices;
 		m_colors = new Color[m_vertices.Length];
         this.worldPosVertices = new Vector3[m_vertices.Length];
-        for (int i=0; i< m_vertices.Length; i++) {
-            this.worldPosVertices[i] = fogOfWarPlane.transform.TransformPoint(m_vertices[i]);
-        }
 
         playerMapVisibility = new MapVisibility[m_vertices.Length];
+
+        for (int i=0; i< m_vertices.Length; i++) {
+            this.worldPosVertices[i] = fogOfWarPlane.transform.TransformPoint(m_vertices[i]);
+            this.playerMapVisibility[i] = new MapVisibility(1.0f, Visibility.INVISIBLE);
+        }
+
 
 		for (int i=0; i < m_colors.Length; i++) {
 			m_colors[i] = Color.black;
@@ -127,10 +171,14 @@ public class FogOfWarGenerator : MonoBehaviour {
             }
         }
 
-        return this.playerMapVisibility[closestVertexIndex] == MapVisibility.VISIBLE;
+        return this.playerMapVisibility[closestVertexIndex].visibility == Visibility.VISIBLE;
     }
 	
 	void UpdateColor() {
+        for (int i = 0; i < this.playerMapVisibility.Length; i++) {
+            m_colors[i].a = this.playerMapVisibility[i].alpha;
+        }
+
 		m_mesh.colors = m_colors;
 	}
 }
